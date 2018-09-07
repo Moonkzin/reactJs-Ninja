86681dabe2719979ae44027aebdfa2bad6b1d88b
Lucas <l.duarte.mk@gmail.com>
2018-09-06 21:10:12 -0300

    modularizacao

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 'use strict'
 
-function sum (val1, val2) {
-    return val1 + val2;
-}
+/*
+Não esquecer de dar o require, dessa forma será criado um h1, com o texto 'Titulo',
+e exportando essa função
+*/
+var React = require('react')
 
-module.exports = sum
+var Title = React.createClass({
+    render: function (){
+        return React.createElement('h1', null, 'Título')
+    }
+})
+
+module.exports = Title
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 'use trict'
 
-console.log('webpack está funcionando!!')
+/*
+Não esquecer de dar o require nas classes necessárias.
+o ReactDOM.Render, serve para fazer aparecer na tela
+e virá dentro da div que tenha o data-js="app"
+*/
 
-var sum = require('./app')
+var Title = require('./app')
+var React = require('react')
+var ReactDOM = require('react-dom')
 
-console.log(sum(1,2))
+console.log('webpack está funcionando!!!!!!!')
+
+
+ReactDOM.render(
+    React.createElement(Title),
+    document.querySelector('[data-js="app"]')
+)
